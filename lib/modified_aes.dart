9d70e3e7d89137b1aaacc8aad466f785109ec70c
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-12-03 19:11:02 +0530

    changes

--- lib/modified_aes.dart
+++ lib/modified_aes.dart
@@ -12,12 +12,10 @@ class Modified_aes {
     var crypt = AesCrypt(secretKey);
     try {
       //aes
-      await crypt.encryptTextToFile(data, path);
-      Uint8List dec = await crypt.decryptDataFromFile(path);
-      String s = String.fromCharCodes(dec);
+      String dec = await crypt.encryptTextToFile(data, path);
       RC4 rc4 = new RC4(secretKey);
       //rc4
-      var bytes = rc4.encodeBytes(utf8.encode(s));
+      var bytes = rc4.encodeBytes(utf8.encode(dec));
       return String.fromCharCodes(bytes);
     } catch (e) {
       print("Encryption failed!");
@@ -26,13 +24,13 @@ class Modified_aes {
 
   Future<String> modified_Decryption(String s, String path, File saveFile,
       String secretKey, List<int> dataStore) async {
-    print("Task Done");
     var crypt = AesCrypt(secretKey);
     RC4 rc4 = new RC4(secretKey);
     //rc4
     var bytes = rc4.decodeBytes(dataStore);
     //aes
     Uint8List dec = await crypt.decryptDataFromFile(bytes);
-    return String.fromCharCodes(dec);;
+    return String.fromCharCodes(dec);
+    ;
   }
 }
